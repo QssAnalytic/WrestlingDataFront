70e0e88a50e11d7e18f36dd1f5c717b3667e8550
emammad22 <mammadoveltun22@gmail.com>
2023-12-18 17:02:07 +0400

    form updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { useState } from "react";
 import SelectBox from "./components/SelectBox";
 
 export default function App() {
+
+  const [success, setSuccess] = useState(false)
+
   const [openSelect, setOpenSelect] = useState({
     action: false,
     techniques: false,
@@ -19,6 +22,10 @@ export default function App() {
     });
   };
 
+  const handleSuccess = (e)=>{
+    setSuccess((prev)=> !prev)
+  }
+
   return (
     <>
       <Header />
@@ -26,8 +33,8 @@ export default function App() {
         <div className="container m-auto">
           <div className="main-inner">
             <div className="action-form text-white bg-wSecMain border border-wGreen rounded-md py-5 px-10">
-              <form className="w-full">
-                <div className="action-left basis-[40%] flex flex-col gap-5">
+              <form className="w-full flex justify-between">
+                <div className="action-left basis-[50%] flex flex-col gap-5">
                   <SelectBox
                     toggleSelect={toggleSelect}
                     openSelect={openSelect}
@@ -38,18 +45,58 @@ export default function App() {
                     openSelect={openSelect}
                     id={"techniques"}
                   />
-                  <div className="left-bottom">
+                  <div className="left-bottom flex justify-between">
                     <SelectBox
                       toggleSelect={toggleSelect}
                       openSelect={openSelect}
                       id={"score"}
+                      ok
                     />
-                    <div className="second-container">
-                      
+                    <div className="second-container flex items-center">
+                      <label htmlFor="second">Second:</label>
+                      <div className="time flex justify-between px-3 py-5 bg-wMain gap-3">
+                        <div className="minute basis-[50%]">
+                          <input
+                            type="text"
+                            className="w-[2.75rem] h-[1.875rem] rounded outline-none bg-[#D9D9D9] bg-opacity-10 text-white text-center"
+                          />
+                        </div>
+                        <p>:</p>
+                        <div className="second">
+                          <input
+                            type="text"
+                            className="w-[2.75rem] h-[1.875rem] rounded outline-none bg-[#D9D9D9] bg-opacity-10 text-white text-center"
+                          />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="action-right basis-[40%]"></div>
+                <div className="action-right flex flex-col basis-[40%] gap-7 rounded">
+                  <div className="right-top pt-3 pb-11 px-14 bg-wMain flex w-full justify-around">
+                    <div className="success-container flex flex-col basis-[45%] gap-3">
+                      <p className="text-center">Successfull : </p>
+                      <div className="success rounded-[3.5rem] py-3 px-3 bg-[#1E264B]">
+                        <div className={`${success ? `translate-x-[80px] transition-all` : 'translate-x-0 transition-all'} yes-no h-[3rem] w-[4rem] rounded-[3.5rem] bg-wGreen flex items-center justify-center`} onClick={handleSuccess}>
+                          Yes
+                        </div>
+                      </div>
+                    </div>
+                    <div className="success-container flex flex-col basis-[45%] gap-3">
+                      <p className="text-center">Defense Reason : </p>
+                      <div className="success rounded-[3.5rem] py-3 px-3 bg-[#1E264B]">
+                        <div className="yes-no h-[3rem] w-[4rem] rounded-[3.5rem] bg-wGreen flex items-center justify-center">
+                          Yes
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="right-bottom self-end">
+                    <button className="btn-action w-[19rem] h-[3.125rem] bg-wBlue p-4 rounded text-[#C9D4EA]">
+                      Submit
+                    </button>
+                  </div>
+                </div>
               </form>
             </div>
             <Table />
--- src/components/SelectBox.jsx
+++ src/components/SelectBox.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { IoCaretDownOutline } from "react-icons/io5";
+import SelectList from "./SelectList";
 
 
-export default function SelectBox({toggleSelect, openSelect, id}) {
+export default function SelectBox({toggleSelect, openSelect, id, ok}) {
   return (
     <>
-      <div className="select-action flex items-center rounded w-[40%] gap-[1.88rem]">
-        <label htmlFor="action">{id} :</label>
+      <div className={`select-action flex items-center rounded w-[100%] gap-[1.78rem] ${ok ? 'w-[39%]' : 'w-[100%]'}`}>
+        <label htmlFor="action">{id}: </label>
         <div
-          className="select-box flex justify-between cursor-pointer bg-wMain px-5 py-4 relative"
+          className="select-box flex justify-between cursor-pointer w-[100%] bg-wMain px-5 py-4 relative"
           id={id}
           onClick={toggleSelect}
         >
@@ -16,17 +17,7 @@ export default function SelectBox({toggleSelect, openSelect, id}) {
           <button className="down-select" id="action" onClick={toggleSelect}>
             <IoCaretDownOutline />
           </button>
-          <div
-            className={`drop-select-box absolute top-14 w-full font-light left-0 ${
-              !openSelect[id] ? "hidden" : "block"
-            }`}
-          >
-            <ul>
-              <li className="select-item bg-[#2E4E8F] py-4 px-5">Takedown</li>
-              <li className="select-item bg-[#2E4E8F] py-4 px-5">Takedown</li>
-              <li className="select-item bg-[#2E4E8F] py-4 px-5">Takedown</li>
-            </ul>
-          </div>
+          <SelectList openSelect={openSelect} id={id}/>
         </div>
       </div>
     </>
