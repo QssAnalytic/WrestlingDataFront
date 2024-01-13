24d76f06426cb996ed572490a1d8e1bdc2f97f7d
emammad22 <mammadoveltun22@gmail.com>
2024-01-14 02:13:38 +0400

    configured delete action

--- src/services/api/requests.js
+++ src/services/api/requests.js
@@ -4,6 +4,6 @@ const getData = async (path) => (await instance.get(path)).data;
 const postData = async (path, arg, config) =>
   (await instance.post(path, arg, config)).data;
 
-const updateData = async (path, arg) =>
-  (await instance.put(path, arg)).data;
-export { getData, postData, updateData };
+const updateData = async (path, arg) => (await instance.put(path, arg)).data;
+const deleteData = async (path) => (await instance.delete(path)).data;
+export { getData, postData, updateData, deleteData };
